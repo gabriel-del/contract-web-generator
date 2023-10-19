3151ab403737c34676caa02226561fd385ff189e
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-18 23:58:48 -0300

    reduce script.js

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 import * as latex from './latex.js'
-
-
 document.getElementById('editor').innerHTML = latex.code
-
 const editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/latex");
 editor.session.setUseWrapMode(true);
 editor.setFontSize(18);
 
-const globalEn = new PdfTeXEngine();
 
 document.addEventListener("DOMContentLoaded", async () => {
+  const globalEn = await new PdfTeXEngine();  
   await globalEn.loadEngine();
   compile.addEventListener('click', async () => {
-    if(!globalEn.isReady()) {
-        console.log("Engine not ready yet");
-        return;
-    }
+
     compile.disabled = true;
     compile.innerHTML = "Compiling...";
 
@@ -30,10 +24,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (r.status === 0) {
         const pdfblob = new Blob([r.pdf], {type : 'application/pdf'});
         const objectURL = URL.createObjectURL(pdfblob);
-        setTimeout(()=>{
-            URL.revokeObjectURL(objectURL);
-        }, 30000);
-        console.log(objectURL);
+        setTimeout(()=> URL.revokeObjectURL(objectURL) , 30000);
+        console.log(objectURL)
         pdfbox.innerHTML = `<embed src="${objectURL}" width="100%" height="400px" type="application/pdf">`;
     }
   })
