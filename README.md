229a7ecf00cccdf988da84d75ec45d4bf9fa1467
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-20 05:19:07 -0300

    add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gerador automático de contratos
+
+## Todo
+
+* Navbar to change layout
+* Form with validations
+* Form with CRUD on objects
+* fix "dataFinal", "enderecoC"
+* Save on localStorage
+* Change colors
--- script.js
+++ script.js
@@ -24,19 +24,19 @@ console.log(dbg.objetos)
   await globalEn.loadEngine();
   compile.addEventListener('click', async () => {
     compile.disabled = true;
-    compile.innerHTML = "Compiling...";
+    compile.innerHTML = "Compilando...";
     globalEn.writeMemFSFile("main.tex", editor.getValue());
     globalEn.setEngineMainFile("main.tex");
     let r = await globalEn.compileLaTeX();
     log.innerHTML = r.log;
-    compile.innerHTML = "Compile";
+    compile.innerHTML = "Compilar";
     compile.disabled = false;
     if (r.status === 0) {
       const pdfblob = new Blob([r.pdf], {type : 'application/pdf'});
       const objectURL = URL.createObjectURL(pdfblob);
       setTimeout(()=> URL.revokeObjectURL(objectURL) , 30000);
-      // download.href = objectURL
-      // download.download = "abc"
+      download.href = objectURL
+      download.download = db.nome
       // download.click()
       pdfbox.innerHTML = `<embed src="${objectURL}" width="100%" height="400px" type="application/pdf">`;
     }
