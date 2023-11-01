153a3eadd58996f7bc9a7316b656d0e83711376b
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-31 23:25:22 -0300

    ng generate component --flat --type form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Form } from './form';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form
   ],
   imports: [
     BrowserModule,
