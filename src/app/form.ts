b011ec7fde50f0a543be50932bbc3216c1df6fa0
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-09 22:33:24 -0300

    validation name and form

--- src/app/form.ts
+++ src/app/form.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,6 +8,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   <label>Nome: <input type="text" formControlName="nome"  placeholder="Nome" ></label><br/>
   <label>Email: <input type="email" formControlName="email" placeholder="nome@email.com" ></label><br/>
   <button type="submit">Submit</button>
+  <button (click)="resetar()">Cancelar</button>
   <app-debug [form]="formulario"></app-debug>
 </form>
   `,
@@ -20,13 +21,17 @@ export class Form implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]]
     })
   }
 
   onSubmit() {
-    console.log(this.formulario.value)
+    console.log(this.formulario)
+    // this.formulario.reset()
+  }
+
+  resetar() {
     this.formulario.reset()
   }
 
