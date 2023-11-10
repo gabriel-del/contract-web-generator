af316bd5a20815c352eb77d2bf88dee206fcce7a
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-09 21:42:40 -0300

    add debug component

---
+++ src/app/debug.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-debug',
+  templateUrl: `
+  <div style="margin-top: 20px" *ngIf="form" >
+  <div>Detalhes do form</div>
+  <pre>Form válido: {{ form.valid }}</pre>
+  <!--pre>Form submetido: {{ form.submitted }}</pre -->
+  <pre>Valores: <br>{{ form.value | json }}</pre>
+</div>
+  `
+})
+export class Debug implements OnInit {
+
+  @Input() form;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
