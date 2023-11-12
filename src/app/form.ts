d0aa8ddb3fa16aea2ac39d97dbd7dde69db93d60
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-11 22:02:01 -0300

    add other fields to form

--- src/app/form.ts
+++ src/app/form.ts
@@ -4,18 +4,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   template: `
- <form [formGroup]="formulario" (ngSubmit)="onSubmit()">
+<form [formGroup]="formulario" (ngSubmit)="onSubmit()">
  <div [ngClass]="hasErrorStyle('nome')">
-  <label>Nome: <input type="text" formControlName="nome"  placeholder="Nome" ></label><br/>
-  <app-error-msg [show]="hasError('nome', 'required')">Nome é obrigatório</app-error-msg>
-  <app-error-msg [show]="hasError('nome', 'minlength')">Ao menos 3 caracteres</app-error-msg>
-</div>
-<div [ngClass]="hasErrorStyle('email')">
-  <label>Email: <input type="email" formControlName="email" placeholder="nome@email.com" ></label><br/>
-  <app-error-msg [show]="hasError('email', 'required')">Email é obrigatório</app-error-msg>
-  <app-error-msg [show]="hasError('email', 'email')">Email inválido</app-error-msg>
+    <label>Nome: <input type="text" formControlName="nome"  placeholder="Nome" ></label><br/>
+    <app-error-msg [show]="hasError('nome', 'required')">Nome é obrigatório</app-error-msg>
+    <app-error-msg [show]="hasError('nome', 'minlength')">Ao menos 3 caracteres</app-error-msg>
   </div>
-  <button type="submit">Submit</button>
+  <div [ngClass]="hasErrorStyle('email')">
+    <label>Email: <input type="email" formControlName="email" placeholder="nome@email.com" ></label><br/>
+    <app-error-msg [show]="hasError('email', 'required')">Email é obrigatório</app-error-msg>
+    <app-error-msg [show]="hasError('email', 'email')">Email inválido</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('cep')">
+    <label>Cep: <input type="text" formControlName="cep"></label><br/>
+    <app-error-msg [show]="hasError('cep', 'required')">Cep é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('numero')">
+    <label>Número: <input type="text" formControlName="numero"></label><br/>
+    <app-error-msg [show]="hasError('numero', 'required')">Número é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('complemento')">
+    <label>Completo: <input type="text" formControlName="complemento"></label><br/>
+    <app-error-msg [show]="hasError('complemento', 'required')">Completo é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('rua')">
+    <label>Rua: <input type="text" formControlName="rua"></label><br/>
+    <app-error-msg [show]="hasError('rua', 'required')">Rua é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('bairro')">
+    <label>Bairro: <input type="text" formControlName="bairro"></label><br/>
+    <app-error-msg [show]="hasError('bairro', 'required')">Bairro é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('cidade')">
+    <label>Cidade: <input type="text" formControlName="cidade"></label><br/>
+    <app-error-msg [show]="hasError('cidade', 'required')">Cidade é obrigatório</app-error-msg>
+  </div>
+  <div [ngClass]="hasErrorStyle('estado')">
+    <label>Estado: <input type="text" formControlName="estado"></label><br/>
+    <app-error-msg [show]="hasError('estado', 'required')">Estado é obrigatório</app-error-msg>
+  </div>
+  
+  <button type="submit">Enviar</button>
   <button (click)="resetar()">Cancelar</button>
   <app-debug [form]="formulario"></app-debug>
 </form>
@@ -33,7 +62,14 @@ export class Form implements OnInit {
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3)]],
-      email: [null, [Validators.required, Validators.email]]
+      email: [null, [Validators.required, Validators.email]],
+      cep: [null, [Validators.required]],
+      numero: [null, [Validators.required]],
+      complemento: [null, [Validators.required]],
+      rua: [null, [Validators.required]],
+      bairro: [null, [Validators.required]],
+      cidade: [null, [Validators.required]],
+      estado: [null, [Validators.required]],
     })
   }
 
@@ -45,14 +81,14 @@ export class Form implements OnInit {
   resetar() {
     this.formulario.reset()
   }
-  hasError(where, what?) {
+  hasError(where: string, what?: string) {
     let field = this.formulario.controls[where]
     if (field.errors && field.touched)  return !!what ?  field.errors?.[what] :  true
     return false
 
   }
 
-  hasErrorStyle(field) {
+  hasErrorStyle(field: string) {
     return {
       'has-error': this.hasError(field),
       'has-feedback': this.hasError(field)
