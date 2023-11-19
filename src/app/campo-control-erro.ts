db4bcd41aa9f904863489bda0b13ba387f631aa6
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-19 19:20:06 -0300

    reformulate error messages

--- src/app/campo-control-erro.ts
+++ src/app/campo-control-erro.ts
@@ -5,19 +5,19 @@ import { formValidations } from './validations';
 @Component({
   selector: 'app-error-msg',
   template: `
-  <div *ngIf="show" ><ng-content/></div>
+  <div *ngIf="errorMessage != null" >{{ errorMessage}}</div>
 `
 })
 export class ErrorMsg implements OnInit {
-  @Input() show!: boolean;
+  // @Input() show!: boolean;
   @Input() label!: string;
-  @Input() control!: FormControl 
+  @Input() control!: any
   constructor() { }
   ngOnInit() {
   }
   get errorMessage(){
     for (const propertyName in this.control.errors){
-      if(this.control.errors['hasOwnPropriety'](propertyName) && this.control.touched) {
+      if(this.control.errors['hasOwnProperty'](propertyName) && this.control.touched) {
         return formValidations.getErrorMsg(this.label, propertyName, this.control.errors[propertyName])
       }
     }
--- src/app/form.ts
+++ src/app/form.ts
@@ -11,38 +11,38 @@ import { formValidations } from './validations';
 <form [formGroup]="formulario" (ngSubmit)="onSubmit()">
  <div [ngClass]="hasErrorStyle('nome')">
     <label>Nome: <input type="text" formControlName="nome"  placeholder="Nome" ></label><br/>
-    <app-error-msg [show]="hasError('nome', 'required')">Nome é obrigatório</app-error-msg>
-    <app-error-msg [show]="hasError('nome', 'minlength')">Ao menos 3 caracteres</app-error-msg>
+    <app-error-msg [control]="formulario.get('nome')" label="Nome"></app-error-msg>
+    <!-- <app-error-msg [show]="hasError('nome', 'minlength')">Ao menos 3 caracteres</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('email')">
     <label>Email: <input type="email" formControlName="email" placeholder="nome@email.com" ></label><br/>
-    <app-error-msg [show]="hasError('email', 'required')">Email é obrigatório</app-error-msg>
-    <app-error-msg [show]="hasError('email', 'email')">Email inválido</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('email', 'required')">Email é obrigatório</app-error-msg> -->
+    <!-- <app-error-msg [show]="hasError('email', 'email')">Email inválido</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('cep')">
     <label>Cep: <input type="text" formControlName="cep" (blur)="consultaCEP()"></label><br/>
-    <app-error-msg [show]="hasError('cep', 'required')">Cep é obrigatório</app-error-msg>
-    <app-error-msg [show]="hasError('cep', 'cepInvalido')">Cep inválido</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('cep', 'required')">Cep é obrigatório</app-error-msg> -->
+    <!-- <app-error-msg [show]="hasError('cep', 'cepInvalido')">Cep inválido</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('numero')">
     <label>Número: <input type="text" formControlName="numero"></label><br/>
-    <app-error-msg [show]="hasError('numero', 'required')">Número é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('numero', 'required')">Número é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('complemento')">
     <label>Complemento: <input type="text" formControlName="complemento"></label><br/>
-    <app-error-msg [show]="hasError('complemento', 'required')">Complemento é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('complemento', 'required')">Complemento é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('rua')">
     <label>Rua: <input type="text" formControlName="rua"></label><br/>
-    <app-error-msg [show]="hasError('rua', 'required')">Rua é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('rua', 'required')">Rua é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('bairro')">
     <label>Bairro: <input type="text" formControlName="bairro"></label><br/>
-    <app-error-msg [show]="hasError('bairro', 'required')">Bairro é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('bairro', 'required')">Bairro é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('cidade')">
     <label>Cidade: <input type="text" formControlName="cidade"></label><br/>
-    <app-error-msg [show]="hasError('cidade', 'required')">Cidade é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('cidade', 'required')">Cidade é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('estado')">
     <label>Estado: 
@@ -51,11 +51,11 @@ import { formValidations } from './validations';
         <option *ngFor="let estado of estados" [value]="estado.sigla">{{estado.nome}}</option>
       </select>
     </label><br/>
-    <app-error-msg [show]="hasError('estado', 'required')">Estado é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('estado', 'required')">Estado é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('bloco')"> Bloco: 
     <label *ngFor="let item of blocosOp"><input type="radio" [value]="item.valor" formControlName="bloco">{{item.desc}}</label>  
-    <app-error-msg [show]="hasError('bloco', 'required')">Bloco é obrigatório</app-error-msg>
+    <!-- <app-error-msg [show]="hasError('bloco', 'required')">Bloco é obrigatório</app-error-msg> -->
   </div>
   <div [ngClass]="hasErrorStyle('hasEndereco')">
     <label>Tem Endereço?<input type="checkbox" formControlName="hasEndereco"></label><br/>
@@ -65,7 +65,7 @@ import { formValidations } from './validations';
     <div   *ngFor="let item of formulario.get('items')['controls']; let i = index">
       <label>{{ items[i] }}<input type="checkbox" [formControlName]="i"></label><br/>
       <!-- <label>Cama<input type="checkbox" formControlName="items"></label><br/> -->
-      <!-- <app-error-msg [show]="hasError('tems', 'required')">Cidade é obrigatório</app-error-msg> -->
+      <!-- <app-error-msg [show]="hasError('items', 'required')">Cidade é obrigatório</app-error-msg> -->
     </div>
   </div>
   
