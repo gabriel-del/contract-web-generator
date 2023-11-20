5aafac3fae0473a22c7d70ea40668d3bc83cd1a7
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-19 23:56:45 -0300

    update header

--- src/app/views/header.ts
+++ src/app/views/header.ts
@@ -3,11 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+<p>
+  <mat-toolbar color="primary">
+    <mat-toolbar-row>
+      <span>Gerador de contratos</span>
+      <span class="example-spacer"></span>
+      <button mat-icon-button class="example-icon" aria-label="Example icon-button with menu icon">
+        <mat-icon>menu</mat-icon>
+      </button>
+    </mat-toolbar-row>
+
+    <div *ngIf="false">
+
+    <mat-toolbar-row>
+      <span>Formulário</span>
+      <span class="example-spacer"></span>
+    </mat-toolbar-row>
+    <mat-toolbar-row>
+      <span>Histórico</span>
+      <span class="example-spacer"></span>
+    </mat-toolbar-row>
+    <mat-toolbar-row>
+      <span>Desenvolvedor</span>
+      <span class="example-spacer"></span>
+    </mat-toolbar-row>
+  </div>
+  </mat-toolbar>
+</p>
   `,
-  styles: [
+  styles: [`
+  .example-spacer {
+  flex: 1 1 auto;
+}`
   ]
 })
 export class Header {
