e4843c65f344b9a970d5481f5713ef33cd37656e
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-22 12:21:36 -0300

    pass functions to form.service

--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -1,9 +1,116 @@
 import { Injectable } from '@angular/core';
+import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms'
+import { formValidations } from './validations';
+import {distinctUntilChanged, filter, map, switchMap, tap} from 'rxjs/operators'
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
+  items: String[] = ['Cama', 'TV', 'Geladeira', 'Sofá', 'Armário']
+
   myvar = 123
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+  formulario: FormGroup = this.formBuilder.group({
+    nome: [null, [Validators.required, Validators.minLength(3)]],
+    telefone: [null, []],
+    cep: [null, [formValidations.cepValidator]],
+    numero: [null, []],
+    complemento: [null, []],
+    rua: [null, []],
+    bairro: [null, []],
+    cidade: [null, []],
+    estado: [null, []],
+    bloco: [null, []],
+    hasEndereco: [true, []],
+    items: this.buildItems(),
+  })
+
+  buildItems(){
+    const values = this.items.map(v => new FormControl(false))
+    return this.formBuilder.array(values)
+  }
+
+
+
+
+  // resetar() {
+  //   this.formulario.reset()
+  // }
+
+
+  // formulario.get('cep').statusChanges
+  // .pipe(
+  //   distinctUntilChanged(),
+  //   tap( value => console.log("status cep: ", value) ),
+  //   switchMap(status => status === 'VALID' ? 
+  //   this.http.get(`//viacep.com.br/ws/${this.formulario.get('cep').value}/json`) :
+  //   empty()
+  //   )
+  // )
+  // .subscribe(
+  //   (dados: any) => dados ? this.formulario.patchValue({
+  //     rua: dados.logradouro,
+  //     bairro: dados.bairro,
+  //     cidade: dados.localidade,
+  //     estado: dados.uf
+  // }) : {} )
+
+  // formulario.get('estado').valueChanges
+  // .pipe(
+  //   map(estado => this.estados.filter(({sigla}) => sigla === estado)),
+  //   map(estado => estado[0].cidades),
+  // )
+  // .subscribe(cidades => this.cidades = cidades)
+
+  // consultaCEP() {
+  //   const cep = this.formulario.get('cep')?.value?.replace(/\D/g,'')
+  //   if (cep != null && cep !== '' && /^[0-9]{8}$/.test(cep)) 
+  //     this.http.get(`//viacep.com.br/ws/${cep}/json`).subscribe((dados: any)  =>  this.formulario.patchValue({
+  //       rua: dados.logradouro,
+  //       bairro: dados.bairro,
+  //       cidade: dados.localidade,
+  //       estado: dados.uf
+  //   }))
+  // }
+
+  // texRead(){
+  //   // let myvar = this.formulario.get('numero').value
+  //   let myvar = "minha var"
+  //   this.http.get('/assets/main.tex', {responseType: 'text'})
+  //   .pipe(
+  //     map(dados => dados.replaceAll("\\","\\\\").replaceAll("}$", "}")),
+  //     map(dados => eval(`dados = \`${dados}\``) ),
+  //     // tap(dados => console.log(dados))
+  //   )
+  //   .subscribe(dados => this.texContent = dados)
+  // }
+
+  //  async compilar(){
+  //    console.log("Compilar")
+  //    this.compileMsg = "Compilando"
+  //    const globalEn = await new PdfTeXEngine
+  //    await globalEn.loadEngine()
+  //    globalEn.writeMemFSFile("main.tex", this.texContent);
+  //    globalEn.setEngineMainFile("main.tex");
+  //    let r = await globalEn.compileLaTeX();
+  //    this.log = r.log
+  //    this.compileMsg = "Compilar"
+
+  //    if (r.status === 0) {
+  //     let a = document.createElement('a');
+  //     // console.log(r.pdf)
+  //     a.href = r.pdf
+  //     a.download = "12345"
+  //     a.click()
+  //     console.log(a)
+  //     console.log(a.href)
+  //     const pdfblob = new Blob([r.pdf], {type : 'application/pdf'});
+  //     const objectURL = URL.createObjectURL(pdfblob);
+  //     this.pdfBox = objectURL
+  //     // console.log(this.pdfBox)
+  //   }
+  // }
 }
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { DropdownService } from './dropdown.service';
+import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms'
+
 import { EstadoBr } from './model';
-import { formValidations } from './validations';
-import {distinctUntilChanged, filter, map, switchMap, tap} from 'rxjs/operators'
 import { empty } from 'rxjs';
 import {PdfTeXEngine} from './../../assets/PdfTeXEngine.js';
 import {myTest} from './../../assets/custom.js';
@@ -68,19 +67,20 @@ import {FormService} from './form.service'
   <div [ngClass]="hasErrorStyle('hasEndereco')">
     <label>Tem Endereço?<input type="checkbox" formControlName="hasEndereco"></label><br/>
   </div>
-  <div [ngClass]="hasErrorStyle('items')" formArrayName="items"> Items: 
+
+  
+  <!-- <div [ngClass]="hasErrorStyle('items')" formArrayName="items"> Items: 
     <div   *ngFor="let item of formulario.get('items')['controls']; let i = index">
       <label>{{ items[i] }}<input type="checkbox" [formControlName]="i"></label><br/>
-      <!-- <label>Cama<input type="checkbox" formControlName="items"></label><br/> -->
+      <label>Cama<input type="checkbox" formControlName="items"></label><br/>
     </div>
-  </div>
+  </div> -->
   
   <button type="submit">Enviar</button>
-  <button (click)="resetar()">Cancelar</button>
+  <!-- <button (click)="resetar()">Cancelar</button> -->
   <!-- <app-debug [form]="formulario"></app-debug> -->
 </form>
 <button mat-fab extended color="primary" (click)="compilar()">{{compileMsg}}</button>  
-<button mat-fab extended color="primary" (click)="showVar()">show var</button>  
 <div class="app-ace-editor"
      style="width: 500px;height: 250px;"
      #editor id="editor">{{texContent}}</div>
@@ -99,7 +99,6 @@ import {FormService} from './form.service'
   }`]
 })
 export class Form implements OnInit {
-  formulario!: FormGroup
   estados!: any
   log!: any
   pdfBox: any = ''
@@ -107,12 +106,28 @@ export class Form implements OnInit {
   cidades!: any[]
   texContent: string = 'jkjkj'
   blocosOp!: any[]
-  items: any[] = ['Cama', 'TV', 'Geladeira', 'Sofá', 'Armário']
 
-  constructor(private formBuilder: FormBuilder,
+
+  formulario: FormGroup = this.formBuilder.group({
+    nome: [null, [Validators.required, Validators.minLength(3)]],
+    telefone: [null, []],
+    cep: [null, []],
+    numero: [null, []],
+    complemento: [null, []],
+    rua: [null, []],
+    bairro: [null, []],
+    cidade: [null, []],
+    estado: [null, []],
+    bloco: [null, []],
+    hasEndereco: [true, []],
+    items: [null, []],
+  })
+
+  constructor(
     private http: HttpClient,
     private dropdownService: DropdownService,
-    private formService: FormService
+    private formService: FormService,
+    private formBuilder: FormBuilder
     ){}
 
   ngOnInit(): void {
@@ -123,7 +138,6 @@ export class Form implements OnInit {
 
     console.log(this.formService.myvar)
 
-    this.texRead()
     ace.config.set("fontSize", "14px");
     ace.config.set('basePath', "https://ace.c9.io/build/src-noconflict/")
 
@@ -137,63 +151,23 @@ export class Form implements OnInit {
       this.texContent = aceEditor.getValue()
     });
 
-      console.log("texx:")
-      console.log(this.texContent)
-    this.formulario = this.formBuilder.group({
-      nome: [null, [Validators.required, Validators.minLength(3)]],
-      telefone: [null, []],
-      cep: [null, [formValidations.cepValidator]],
-      numero: [null, []],
-      complemento: [null, []],
-      rua: [null, []],
-      bairro: [null, []],
-      cidade: [null, []],
-      estado: [null, []],
-      bloco: [null, []],
-      hasEndereco: [true, []],
-      items: this.buildItems(),
-    })
 
-    this.formulario.get('cep').statusChanges
-    .pipe(
-      distinctUntilChanged(),
-      tap( value => console.log("status cep: ", value) ),
-      switchMap(status => status === 'VALID' ? 
-      this.http.get(`//viacep.com.br/ws/${this.formulario.get('cep').value}/json`) :
-      empty()
-      )
-    )
-    .subscribe(
-      (dados: any) => dados ? this.formulario.patchValue({
-        rua: dados.logradouro,
-        bairro: dados.bairro,
-        cidade: dados.localidade,
-        estado: dados.uf
-    }) : {} )
 
-    this.formulario.get('estado').valueChanges
-    .pipe(
-      map(estado => this.estados.filter(({sigla}) => sigla === estado)),
-      map(estado => estado[0].cidades),
-    )
-    .subscribe(cidades => this.cidades = cidades)
+
+ 
   }
 
-  showVar(){
-    this.formService.myvar = 3
-    console.log(this.formService.myvar)
+  
+  compilar(){
+    console.log("compilou")
   }
 
-  buildItems(){
-    const values = this.items.map(v => new FormControl(false))
-    return this.formBuilder.array(values)
-  }
 
   onSubmit() {
     let valueSubmit = Object.assign({}, this.formulario.value)
-    valueSubmit = Object.assign(valueSubmit, {
-      items: valueSubmit.items.map((v,i) => v ? this.items[i] : null).filter(v => v !== null)
-    })
+    // valueSubmit = Object.assign(valueSubmit, {
+    //   items: valueSubmit.items.map((v,i) => v ? this.items[i] : null).filter(v => v !== null)
+    // })
 
     console.log(valueSubmit)
     if (this.formulario.valid) {
@@ -204,9 +178,7 @@ export class Form implements OnInit {
     }
   }
 
-  resetar() {
-    this.formulario.reset()
-  }
+
   hasError(where: string, what?: string) {
     let field = this.formulario.controls[where]
     if (field.errors && (field.touched || field.dirty))  return !!what ?  field.errors?.[what] :  true
@@ -221,59 +193,8 @@ export class Form implements OnInit {
     }
   }
 
-  consultaCEP() {
-    const cep = this.formulario.get('cep')?.value?.replace(/\D/g,'')
-    if (cep != null && cep !== '' && /^[0-9]{8}$/.test(cep)) 
-      this.http.get(`//viacep.com.br/ws/${cep}/json`).subscribe((dados: any)  =>  this.formulario.patchValue({
-        rua: dados.logradouro,
-        bairro: dados.bairro,
-        cidade: dados.localidade,
-        estado: dados.uf
-    }))
-  }
-
-  texRead(){
-    // let myvar = this.formulario.get('numero').value
-    let myvar = "minha var"
-    this.http.get('/assets/main.tex', {responseType: 'text'})
-    .pipe(
-      map(dados => dados.replaceAll("\\","\\\\").replaceAll("}$", "}")),
-      map(dados => eval(`dados = \`${dados}\``) ),
-      // tap(dados => console.log(dados))
-    )
-    .subscribe(dados => this.texContent = dados)
-  }
-
-   async compilar(){
-     console.log("Compilar")
-     this.compileMsg = "Compilando"
-     const globalEn = await new PdfTeXEngine
-     await globalEn.loadEngine()
-     globalEn.writeMemFSFile("main.tex", this.texContent);
-     globalEn.setEngineMainFile("main.tex");
-     let r = await globalEn.compileLaTeX();
-     this.log = r.log
-     this.compileMsg = "Compilar"
-
-     if (r.status === 0) {
-      let a = document.createElement('a');
-      // console.log(r.pdf)
-      a.href = r.pdf
-      a.download = "12345"
-      a.click()
-      console.log(a)
-      console.log(a.href)
-      const pdfblob = new Blob([r.pdf], {type : 'application/pdf'});
-      const objectURL = URL.createObjectURL(pdfblob);
-      this.pdfBox = objectURL
-      // console.log(this.pdfBox)
-    }
-
-  
-      // console.log(globalEn.loadEngine)
 
 
-  }
 
 
 
