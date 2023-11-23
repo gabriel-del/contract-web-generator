c9b798c5e37820641c3deb8fce29f88a010fb511
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-23 14:45:29 -0300

    organize inputs

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -14,27 +14,17 @@ import { EstadoBr } from './model';
 
 <div>
 
-<mat-form-field>
-  <mat-label>Objetos</mat-label>
-  <textarea matInput></textarea>
-</mat-form-field>
 
 
 <p>
 Bloco:
-<mat-button-toggle-group  aria-label="Font Style">
+<mat-button-toggle-group formControlName="bloco" aria-label="Font Style">
     <mat-button-toggle value="a">A</mat-button-toggle>
     <mat-button-toggle value="b">B</mat-button-toggle>
     <mat-button-toggle value="c">C</mat-button-toggle>
   </mat-button-toggle-group>
   </p>
 
-
-  <mat-form-field >
-    <mat-label>Aluguel: </mat-label>
-    <span matPrefix>R$ &nbsp;</span>
-    <input matInput type="number" placeholder="Ex.: 1200">
-  </mat-form-field>
 
 <mat-form-field>
   <mat-label>Apartamento</mat-label>
@@ -43,88 +33,115 @@ Bloco:
   </mat-select>
 </mat-form-field>
 
+  <mat-form-field >
+    <mat-label>Aluguel: </mat-label>
+    <span matPrefix>R$ &nbsp;</span>
+    <input matInput type="number" placeholder="Ex.: 1200">
+  </mat-form-field>
+
+
+
+
 <mat-form-field>
-  <mat-label>Vencimento</mat-label>
+  <mat-label>Data de Início</mat-label>
+  <input matInput [matDatepicker]="picker">
+  <mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
+  <mat-datepicker #picker></mat-datepicker>
+</mat-form-field>
+
+<mat-form-field>
+  <mat-label>Dia de vencimento</mat-label>
   <mat-select >
     <mat-option *ngFor="let dia of vencimento" value="dia">{{dia}}</mat-option>
   </mat-select>
 </mat-form-field>
 
-<mat-form-field>
+
+  <mat-form-field>
+  <mat-label>Objetos</mat-label>
+  <textarea matInput></textarea>
+</mat-form-field>
+
+
+  </div><div>
+    <!-- ###### DIVISÃO -->
+
+    <mat-form-field [ngClass]="hasErrorStyle('nome')">
+     <mat-label>Nome: </mat-label>
+     <input type="text" matInput formControlName="nome">
+    <app-error-msg [control]="formulario.get('nome')" label="Nome"></app-error-msg>
+</mat-form-field>
+
+
+    <mat-form-field>
   <mat-label>Estado Civil</mat-label>
   <mat-select >
     <mat-option *ngFor="let option of estadoCivil" value="option">{{option}}</mat-option>
   </mat-select>
 </mat-form-field>
 
-
-<mat-form-field [ngClass]="hasErrorStyle('nome')">
-     <mat-label>Nome: </mat-label>
-     <input type="text" matInput formControlName="nome">
-    <app-error-msg [control]="formulario.get('nome')" label="Nome"></app-error-msg>
-</mat-form-field>
-
-  <mat-form-field>
+<mat-form-field>
     <mat-label>Telefone</mat-label>
     <span matPrefix>+55 &nbsp;</span>
     <input type="tel" matInput formControlName="telefone" placeholder="81 91111-1111">
   </mat-form-field>
 
-  </div>
-  <div>
 
+ <!-- <div [ngClass]="hasErrorStyle('hasEndereco')"> -->
+ <mat-slide-toggle color="primary" formControlName="hasEndereco">Tem endereço?</mat-slide-toggle>
 
-  <!-- <mat-form-field [ngClass]="hasErrorStyle('cep')"> -->
-    <mat-label>Cep:</mat-label>
-     <input type="text" formControlName="cep">
+<ng-container *ngIf="formulario.get('hasEndereco').value">
+  <mat-form-field>
+    <mat-label>Cep: </mat-label>
+    <input type="number" matInput formControlName="cep" placeholder="11111111">
     <app-error-msg [control]="formulario.get('cep')" label="Cep"></app-error-msg>
-  <!-- </mat-form-field> -->
-  <div [ngClass]="hasErrorStyle('numero')">
-    <label>Número: <input type="text" formControlName="numero"></label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('complemento')">
-    <label>Complemento: <input type="text" formControlName="complemento"></label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('rua')">
-    <label>Rua: <input type="text" formControlName="rua"></label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('bairro')">
-    <label>Bairro: <input type="text" formControlName="bairro"></label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('cidade')">
-    <label>Cidade: 
-      <select formControlName="cidade">
-        <option *ngFor="let cidade of cidades" [value]="cidade">{{cidade}}</option>
-      </select>
-    </label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('estado')">
-    <label>Estado: 
-      <select formControlName="estado">
-        <option *ngFor="let estado of estados" [value]="estado.sigla">{{estado.nome}}</option>
-      </select>
-    </label><br/>
-  </div>
-  <div [ngClass]="hasErrorStyle('bloco')"> Bloco: 
-    <label *ngFor="let item of blocosOp"><input type="radio" [value]="item.valor" formControlName="bloco">{{item.desc}}</label>  
-  </div>
-
-
-  <mat-slide-toggle color="primary">Endereço?</mat-slide-toggle>
-
-  <div [ngClass]="hasErrorStyle('hasEndereco')">
-    <label>Tem Endereço?<input type="checkbox" formControlName="hasEndereco"></label><br/>
-  </div>
-
+  </mat-form-field>
 
 
   <mat-form-field>
-  <mat-label>Choose a date</mat-label>
-  <input matInput [matDatepicker]="picker">
-  <mat-hint>MM/DD/YYYY</mat-hint>
-  <mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
-  <mat-datepicker #picker></mat-datepicker>
-</mat-form-field>
+    <mat-label>Número: </mat-label>
+    <input type="number" matInput formControlName="numero" placeholder="00">
+    <app-error-msg [control]="formulario.get('numero')" label="Número"></app-error-msg>
+  </mat-form-field>
+
+  <mat-form-field>
+    <mat-label>Complemento: </mat-label>
+    <input type="text" matInput formControlName="complemento" placeholder="Ex.: apt 01">
+    <app-error-msg [control]="formulario.get('complemento')" label="Complemento"></app-error-msg>
+  </mat-form-field>
+
+  <mat-form-field>
+    <mat-label>Rua: </mat-label>
+    <input type="text" matInput formControlName="rua" placeholder="Ex.: Cavalo Marinho">
+    <app-error-msg [control]="formulario.get('rua')" label="Rua"></app-error-msg>
+  </mat-form-field>
+
+  <mat-form-field>
+    <mat-label>Bairro: </mat-label>
+    <input type="text" matInput formControlName="bairro" placeholder="Ex.: Porto de Galinhas">
+    <app-error-msg [control]="formulario.get('bairro')" label="Bairro"></app-error-msg>
+  </mat-form-field>
+  
+  <mat-form-field>
+    <mat-label>Cidade: </mat-label>
+    <mat-select formControlName="cidade">
+      <mat-option *ngFor="let cidade of cidades" [value]="cidade">{{cidade}}</mat-option>
+    </mat-select>
+    <app-error-msg [control]="formulario.get('cidade')" label="Cidade"></app-error-msg>  
+  </mat-form-field>
+  
+  <mat-form-field>
+    <mat-label>Estado: </mat-label>
+    <mat-select formControlName="estado">
+      <mat-option *ngFor="let estado of estados" [value]="estado.sigla">{{estado.nome}}</mat-option>
+    </mat-select>
+    <app-error-msg [control]="formulario.get('estado')" label="Estado"></app-error-msg>  
+  </mat-form-field>
+  
+  </ng-container>
+
+ 
+  
 
 
 
