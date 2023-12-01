771318b0779a29327946fb2cbac232e845a2de40
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-11-30 23:21:07 -0300

    pdf working

--- src/app/components/pdf.ts
+++ src/app/components/pdf.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormService} from '../form/form.service'
 
 @Component({
   selector: 'app-pdf',
   template: `
-    <object *ngIf="pdf != ''" [data]= "pdf | safe" width="800" height="500"> </object> 
-    
-
+    <object *ngIf="pdf" [data]= "pdf | safe" width="800" height="500"> </object> 
   `,
   styles: [
   ]
 })
-export class Pdf {
+export class Pdf implements OnInit{
   constructor( private formService: FormService ){}
 r = this.formService.r
-pdf :any
+pdf :any = null
+
+ngOnInit(): void {
+  this.formService.compiling$
+  // .pipe( tap(v => console.log(v)) )
+  .subscribe(a => {if(!a) this.updatePdf()})
+}
 
 updatePdf(){
-  if (this.r.status === 0) {
-    this.pdf = this.r.pdf
+  if (this.formService.r?.status === 0) {
+    this.pdf = this.formService.r.pdf
     const pdfBlob = new Blob([this.pdf], {type : 'application/pdf'});
     const objectURL = URL.createObjectURL(pdfBlob);
     let a = document.createElement('a');
