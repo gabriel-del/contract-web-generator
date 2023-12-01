7a349feae6ad35bf4fb4083dd9e1d57ed4ec1f10
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-12-01 00:15:38 -0300

    improve dev view

--- src/app/views/dev.ts
+++ src/app/views/dev.ts
@@ -5,9 +5,12 @@ import {FormService} from '../form/form.service'
 @Component({
   selector: 'app-dev',
   template: `
+  <app-editor [(text)]="tex" mode="latex" ></app-editor>
   <app-form-submit></app-form-submit>
+  <app-log></app-log>
+    <app-pdf showPdf=true savePdf=false></app-pdf>
     <app-debug-form></app-debug-form>
-    <app-editor [(text)]="tex" mode="latex" ></app-editor>
+    <app-debug-f></app-debug-f> 
 
   `,
   styles: [
