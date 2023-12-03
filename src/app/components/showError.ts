e96d21714943440607926f1094c072fdd7a8a36e
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-12-03 10:28:31 -0300

    rm check of hasOwnProperty

--- src/app/components/showError.ts
+++ src/app/components/showError.ts
@@ -16,8 +16,7 @@ export class ShowError implements OnInit {
   }
   get errorMessage() {
     for (const validator in this.control.errors) {
-      // if (this.control.errors.hasOwnProperty(validator) && this.control.touched)
-      if (Object.prototype.hasOwnProperty.call(this.control.errors, validator) && this.control.touched)
+      if (this.control.touched)
         return formValidations.getErrorMsg(validator, this.label, this.control.errors[validator])
     }
     return null
